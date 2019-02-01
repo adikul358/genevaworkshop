053f34e38155afac2c784bec1999a27b44965810
adikul358 <38763258+adikul358@users.noreply.github.com>
2019-02-01 14:28:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
 	<section id="intro">
 		<div class="intro-container wow fadeIn">
 			<h1 class="mb-4 pb-0">CERN South Asia Science Education Programme</h1>
-			<h3 class="mb-4 pb-0">In Association with Life Lab Education and Research Foundation and Shiv Nadar Foundation</h3>
+			<p class="intro-assoc mb-4 pb-0">In Association with Life Lab Education and Research Foundation and Shiv Nadar Foundation</p>
 			<!--<p class="mb-4 pb-0">April 2019</p>-->
 			<!-- <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a> -->
 			<a href="#about" class="about-btn scrollto">About The Event</a>
