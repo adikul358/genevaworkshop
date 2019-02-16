951bbd7817613b87d7fc9040788786e6907b6e87
adikul358 <adi.kul358@gmail.com>
2019-02-17 05:19:05 +0530

    homepage layout

--- committee/index.php
+++ committee/index.php
@@ -5,133 +5,40 @@
 	<meta charset="utf-8">
 	<title>CERN South Asia Science Education Program</title>
 	<meta content="width=device-width, initial-scale=1.0" name="viewport">
-	<link href="img/favicon.ico" rel="icon">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800"
+	<link href="/img/favicon.ico" rel="icon">
+	<link href="/https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800"
 	 rel="stylesheet">
-	<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-	<link href="lib/animate/animate.min.css" rel="stylesheet">
-	<link href="lib/venobox/venobox.css" rel="stylesheet">
-	<link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-	<link href="css/style.css" rel="stylesheet">
+	<link href="/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<link href="/lib/animate/animate.min.css" rel="stylesheet">
+	<link href="/lib/venobox/venobox.css" rel="stylesheet">
+	<link href="/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+	<link href="/css/style.css" rel="stylesheet">
 </head>
 
 <body>
 	<header id="header">
 		<div class="container">
 			<div id="logo" class="pull-left">
-				<a href="#intro" class="scrollto">
+				<a href="/#intro" class="scrollto">
 					<!-- <h1 class="mb-4 pb-0"><span>Kanona</span></h1> -->
 				</a>
 			</div>
 
 			<nav id="nav-menu-container">
 				<ul class="nav-menu">
-					<li class="menu-active"><a href="#intro">Home</a></li>
-					<li><a href="#about">About</a></li>
-					<li><a href="#collaborators">Collaborators</a></li>
-					<li><a href="#venue">Venue</a></li>
-					<li><a href="#schedule">Schedule</a></li>
-					<li class="buy-tickets"><a href="#register">Registration</a></li>
+					<li class="menu-active"><a href="/#intro">Home</a></li>
+					<li><a href="/#about">About</a></li>
+					<li><a href="/#collaborators">Collaborators</a></li>
+					<li><a href="/#venue">Venue</a></li>
+					<li><a href="/#schedule">Schedule</a></li>
+					<li class="buy-tickets"><a href="/#register">Registration</a></li>
 				</ul>
 			</nav>
 		</div>
 	</header>
 
-	<section id="intro">
-		<div class="intro-container wow fadeIn">
-			<h1 id="go" class="mb-4 pb-0">CERN South Asia Science Education Program</h1>
-			<p class="intro-subtitle mb-4 pb-0">Certified STEAM Training Program for High-School Teachers</p>
-			<!-- <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a> -->
-			<div class="row no-gutters collaborators-wrap clearfix">
-				<div class="col-xs-3 col-sm-6">
-					<a target="_blank" href="https://home.cern">
-						<div class="collaborator-logo">
-							<img src="img/collaborators/CERN_intro2.jpg" class="img-fluid" alt="CERN">
-						</div>
-					</a>
-				</div>
-				<div class="col-xs-3 col-sm-6">
-					<a target="_blank" href="https://ecolint.ch">
-						<div class="collaborator-logo">
-							<img src="img/collaborators/Ecolint.jpg" class="img-fluid" alt="Ecolint">
-						</div>
-					</a>
-				</div>
-				<div class="col-xs-3 col-sm-6">
-					<a target="_blank" href="http://www.lifelab.org.in/">
-						<div class="collaborator-logo">
-							<img src="img/collaborators/LLF.png" class="img-fluid" alt="LifeLab Foundation">
-						</div>
-					</a>
-				</div>
-				<div class="col-xs-3 col-sm-6">
-					<a target="_blank" href="http://www.shivnadarfoundation.org/">
-						<div class="collaborator-logo">
-							<img src="img/collaborators/SNF.png" class="img-fluid" alt="Shiv Nadar School Noida">
-						</div>
-					</a>
-				</div>
-			</div>
-			<a href="#about" class="about-btn scrollto">About The Event</a>
-		</div>
-	</section>
-
 	<main id="main">
-		<section id="about">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-9">
-						<img style="padding-bottom: 25px" width="100%" src="img/about-bg.jpg" alt="">
-						<h2>About The Event</h2>
-						<style>div#aboutcol { border-right: 1px solid #fff; }</style>
-						<p>
-							This first-of-its-kind certified professional development program for 50 high school teachers of STEM
-							subjects is an international collaborative initiative by CERN, The International School of Geneva
-							(Ecolint) and Life Lab Foundation. Designed and delivered by senior school educators and eminent
-							CERN scientists, this training program will empower teachers with 21st century pedagogical
-							knowledge and skills in interdisciplinary STEAM fields.
-						</p>
-						<div class="row">
-							<div class="col-lg-6" id=aboutcol>
-								<h3>Themes</h3>
-								<p>Anchored around the UN Sustainable Development Goals, the modules will focus on:</p>
-								<ul>
-									<li>Concept-based teaching and learning</li>
-									<li>Project-based experiential learning</li>
-									<li>STEM through arts, movement and mindfulness</li>
-									<li>Addressing inclusion and diversity in the classroom</li>
-								</ul>
-							</div>
-							<div class="col-lg-6">
-								<h3>Learning Outcomes</h3>
-								<p>Participants will gain valuable skills from a wide arena. These include:</p>
-								<ul>
-									<li>Cross-disciplinary teaching</li>
-									<li>Developing student-centred curriculum</li>
-									<li>Incorporating social skills</li>
-									<li>Character development and citizenship</li>
-									<li>Addressing special needs</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3 brief">
-						<div id="brief">
-						<h3>Where</h3>
-						<p>Shiv Nadar School, Noida</p>
-						<h3>When</h3>
-						<p>22-26 April 2019</p>
-						<h3>No. of Seats</h3>
-						<p>50</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-
-
-
 		<section id="collaborators" class="section-bg wow fadeInUp">
 			<div class="container">
 				<div class="section-header">
@@ -157,7 +64,7 @@
 					<div class="col-lg-6 col-md-12 col-xs-6">
 						<a target="_blank" href="https://ecolint.ch">
 							<div class="collaborator-logo">
-								<img src="img/collaborators/Ecolint.jpg" class="img-fluid" alt="Ecolint">
+								<img src="img/collaborators/EcolInt.jpg" class="img-fluid" alt="Ecolint">
 						</a>
 					</div></a>
 					<div class="collaborator-text">
@@ -333,19 +240,11 @@
 								<div class="speaker">
 									<img src="img/speakers/2.jpg" alt="Hubert Hirthe">
 								</div>
-								<h4>Module 2 <span>Hubert Hirthe</span></h4>
+								<h4>Module 2
+									<span>Hubert Hirthe</span>
+								</h4>
 								<p>Principles of Learning and Integrating STEM Education Pedagogy: Problem Solving, Critical Thinking, Capacity
 									Building</p>
-							</div>
-						</div>
-						<div class="row schedule-item">
-							<div class="col-md-2"><time></time></div>
-							<div class="col-md-10">
-								<div class="speaker">
-									<img src="img/speakers/1.jpg" alt="Hubert Hirthe">
-								</div>
-								<h4>Module 3 <span>Hubert Hirthe</span></h4>
-								<p>STEM Project-Based Learning: Authentic & Effective Experiential Teaching and  Learning</p>
 							</div>
 						</div>
 						<div class="row schedule-item">
@@ -360,8 +259,8 @@
 								<div class="speaker">
 									<img src="img/speakers/1.jpg" alt="Brenden Legros">
 								</div>
-								<h4>Module 4 <span>Brenden Legros</span></h4>
-								<p>Education through the Arts, Movement and Mindfulness: Building Competences for a Better World</p>
+								<h4>Module 3 <span>Brenden Legros</span></h4>
+								<!-- <p>Facere provident incidunt quos voluptas.</p> -->
 							</div>
 						</div>
 						<div class="row schedule-item">
@@ -370,18 +269,8 @@
 								<div class="speaker">
 									<img src="img/speakers/2.jpg" alt="Hubert Hirthe">
 								</div>
-								<h4>Module 5 <span>Hubert Hirthe</span></h4>
-								<p>Teaching and Learning for Inclusion: Role of all teachers in SEN & STEM</p>
-							</div>
-						</div>
-						<div class="row schedule-item">
-							<div class="col-md-2"><time></time></div>
-							<div class="col-md-10">
-								<div class="speaker">
-									<img src="img/speakers/1.jpg" alt="Hubert Hirthe">
-								</div>
-								<h4>Module 6 <span>Hubert Hirthe</span></h4>
-								<p>Elementary particle physics in early physics education</p>
+								<h4>Module 4 <span>Hubert Hirthe</span></h4>
+								<!-- <p>Maiores dignissimos neque qui cum accusantium ut sit sint inventore.</p> -->
 							</div>
 						</div>
 						<div class="row schedule-item">
@@ -453,8 +342,7 @@
 									<img src="img/speakers/1.jpg" alt="Brenden Legros">
 								</div>
 								<h4>Module 1 <span>Brenden Legros</span></h4>
-								<p>Concept-Based Teaching and Learning: A Student-Centered Cross-Disciplinary Curriculum linked to the
-									Sustainable Development Goals (SDGs)</p>
+								<!-- <p>Facere provident incidunt quos voluptas.</p> -->
 							</div>
 						</div>
 						<div class="row schedule-item">
@@ -464,18 +352,7 @@
 									<img src="img/speakers/2.jpg" alt="Hubert Hirthe">
 								</div>
 								<h4>Module 2 <span>Hubert Hirthe</span></h4>
-								<p>Principles of Learning and Integrating STEM Education Pedagogy: Problem Solving, Critical Thinking, Capacity
-									Building</p>
-							</div>
-						</div>
-						<div class="row schedule-item">
-							<div class="col-md-2"><time></time></div>
-							<div class="col-md-10">
-								<div class="speaker">
-									<img src="img/speakers/1.jpg" alt="Hubert Hirthe">
-								</div>
-								<h4>Module 3 <span>Hubert Hirthe</span></h4>
-								<p>STEM Project-Based Learning: Authentic & Effective Experiential Teaching and  Learning</p>
+								<!-- <p>Maiores dignissimos neque qui cum accusantium ut sit sint inventore.</p> -->
 							</div>
 						</div>
 						<div class="row schedule-item">
@@ -490,8 +367,8 @@
 								<div class="speaker">
 									<img src="img/speakers/1.jpg" alt="Brenden Legros">
 								</div>
-								<h4>Module 4 <span>Brenden Legros</span></h4>
-								<p>Education through the Arts, Movement and Mindfulness: Building Competences for a Better World</p>
+								<h4>Module 3 <span>Brenden Legros</span></h4>
+								<!-- <p>Facere provident incidunt quos voluptas.</p> -->
 							</div>
 						</div>
 						<div class="row schedule-item">
@@ -500,18 +377,8 @@
 								<div class="speaker">
 									<img src="img/speakers/2.jpg" alt="Hubert Hirthe">
 								</div>
-								<h4>Module 5 <span>Hubert Hirthe</span></h4>
-								<p>Teaching and Learning for Inclusion: Role of all teachers in SEN & STEM</p>
-							</div>
-						</div>
-						<div class="row schedule-item">
-							<div class="col-md-2"><time></time></div>
-							<div class="col-md-10">
-								<div class="speaker">
-									<img src="img/speakers/1.jpg" alt="Hubert Hirthe">
-								</div>
-								<h4>Module 6 <span>Hubert Hirthe</span></h4>
-								<p>Elementary particle physics in early physics education</p>
+								<h4>Module 4 <span>Hubert Hirthe</span></h4>
+								<!-- <p>Maiores dignissimos neque qui cum accusantium ut sit sint inventore.</p> -->
 							</div>
 						</div>
 						<div class="row schedule-item">
