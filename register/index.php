4e2dce69d98f11a1128d62901fe5e3ba12b5369d
adikul358 <adi.kul358@gmail.com>
2019-02-26 12:14:22 +0530

    links

--- register/index.php
+++ register/index.php
@@ -5,22 +5,22 @@
 	<meta charset="utf-8">
 	<title>CERN South Asia Science Education Program</title>
 	<meta content="width=device-width, initial-scale=1.0" name="viewport">
-	<link href="/img/favicon.ico" rel="icon">
-	<link href="/https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800"
+	<link href="/genevaworkshop/img/favicon.ico" rel="icon">
+	<link href="/genevaworkshop/https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800"
 	 rel="stylesheet">
-	<link href="/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<link href="/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-	<link href="/lib/animate/animate.min.css" rel="stylesheet">
-	<link href="/lib/venobox/venobox.css" rel="stylesheet">
-	<link href="/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-	<link href="/css/style.css" rel="stylesheet">
+	<link href="/genevaworkshop/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="/genevaworkshop/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<link href="/genevaworkshop/lib/animate/animate.min.css" rel="stylesheet">
+	<link href="/genevaworkshop/lib/venobox/venobox.css" rel="stylesheet">
+	<link href="/genevaworkshop/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+	<link href="/genevaworkshop/css/style.css" rel="stylesheet">
 </head>
 
 <body>
 	<header id="header" class="header-scrolled">
 		<div class="container">
 			<div id="logo" class="pull-left">
-				<a href="/#intro" class="scrollto">
+				<a href="/genevaworkshop/#intro" class="scrollto">
 					<!-- <h1 class="mb-4 pb-0"><span>Kanona</span></h1> -->
 				</a>
 			</div>
